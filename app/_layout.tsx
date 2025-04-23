c7b51c73e08d9e610eff1df1ecaa54e79f0b97fd
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-04-23 20:28:23 +0100

    feat: enhance tab navigation with icons and theme integration for Home and Profile screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
-import { Stack, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "../utils/theme";
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: true, title: "Home" }} />
-      <Tabs.Screen name="profile" options={{ headerShown: true, title: "Profile" }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen name="index" options={{ headerShown: true, title: "Home", tabBarShowLabel: false , tabBarIcon: ({size, color})=>{ return <Entypo name="leaf" size={size} color={color} />
+
+      }}} />
+      <Tabs.Screen name="profile" options={{ headerShown: true, title: "Profile", tabBarShowLabel: false, tabBarIcon: ({size, color})=>{
+        return <Feather name="user" size={size} color={color} />
+      }}} />
     </Tabs>
   )
 }
