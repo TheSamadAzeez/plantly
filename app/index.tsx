027fc1cd6628a9ec1cdfdb47da96f7fce8ffe865
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-04-23 19:03:03 +0100

    feat: refactor index.tsx and add profile.tsx screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Page() {
+export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is a test</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,21 +14,8 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
