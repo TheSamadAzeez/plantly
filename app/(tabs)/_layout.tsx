f26b9b3a4dee1e72eee779e8246e872652d7ddea
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-04-24 06:55:41 +0100

    feat: add Home and Profile screens with tab navigation and styling

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "@/utils/theme";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen name="index" options={{ title: "Home", tabBarShowLabel: false , tabBarIcon: ({size, color})=>{ return <Entypo name="leaf" size={size} color={color} />
+
+      }}} />
+      <Tabs.Screen name="profile" options={{ title: "Profile", tabBarShowLabel: false, tabBarIcon: ({size, color})=>{
+        return <Feather name="user" size={size} color={color} />
+      }}} />
+    </Tabs>
+  )
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { theme } from '@/utils/theme';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is a test</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
