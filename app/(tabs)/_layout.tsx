cf7d72949fa0e3c5dc048f0d39cd9c97ef7b4410
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-04-24 07:03:46 +0100

    feat: enhance onboarding flow by adding modal presentation and redirect logic in layout components

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,15 @@
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import { theme } from "@/utils/theme";
 
+const hasFinishedOnboarding = false;
+
 export default function Layout() {
+  if (!hasFinishedOnboarding) {
+    return <Redirect href="/onboarding" />;
+  }
+
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
       <Tabs.Screen name="index" options={{ title: "Home", tabBarShowLabel: false , tabBarIcon: ({size, color})=>{ return <Entypo name="leaf" size={size} color={color} />
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false, animation: "fade", presentation: "modal"  }} />
     </Stack>
   );
 }
