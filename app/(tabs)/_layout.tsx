21ec1ef154e4abb9e4bc234dac3e7fe26520195c
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-04-25 07:57:37 +0100

    feat: add new modal screen for creating a plant and enhance tab header with a plus icon for navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,50 @@
-import { Redirect, Tabs } from "expo-router";
-import Entypo from "@expo/vector-icons/Entypo";
-import Feather from "@expo/vector-icons/Feather";
-import { theme } from "@/utils/theme";
-import { useUserStore } from "@/store/userStore";
+import { Link, Redirect, Tabs } from 'expo-router';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import { theme } from '@/utils/theme';
+import { useUserStore } from '@/store/userStore';
+import { Pressable } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Layout() {
   const hasFinishedOnboarding = useUserStore(
-    (state) => state.hasFinishedOnboarding,
+    (state) => state.hasFinishedOnboarding
   );
 
   if (!hasFinishedOnboarding) {
-    return <Redirect href="/onboarding" />;
+    return <Redirect href='/onboarding' />;
   }
 
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
       <Tabs.Screen
-        name="index"
+        name='index'
         options={{
-          title: "Home",
+          title: 'Home',
           tabBarShowLabel: false,
           tabBarIcon: ({ size, color }) => {
-            return <Entypo name="leaf" size={size} color={color} />;
+            return <Entypo name='leaf' size={size} color={color} />;
           },
+          headerRight: () => (
+            <Link href='/new' asChild>
+              <Pressable hitSlop={20} style={{ marginRight: 18 }}>
+                <AntDesign
+                  name='pluscircleo'
+                  size={24}
+                  color={theme.colorGreen}
+                />
+              </Pressable>
+            </Link>
+          ),
         }}
       />
       <Tabs.Screen
-        name="profile"
+        name='profile'
         options={{
-          title: "Profile",
+          title: 'Profile',
           tabBarShowLabel: false,
           tabBarIcon: ({ size, color }) => {
-            return <Feather name="user" size={size} color={color} />;
+            return <Feather name='user' size={size} color={color} />;
           },
         }}
       />
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,14 @@ export default function Layout() {
           presentation: 'modal',
         }}
       />
+      <Stack.Screen
+        name='new'
+        options={{
+          presentation: 'modal',
+          animation: 'slide_from_bottom',
+          title: 'New Plant',
+        }}
+      />
     </Stack>
   );
 }
