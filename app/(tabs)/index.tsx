5cc95946656c97a3d3ada8ac51d1ef61c03e5b94
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-04-27 02:34:08 +0100

    feat: implement FlatList to display plants with PlantCard and navigation for adding new plants

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,29 @@
-import { theme } from "@/utils/theme";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet } from 'react-native';
+import { theme } from '@/utils/theme';
+import { usePlantStore } from '@/store/plantStore';
+import { PlantlyButton } from '@/components/PlantlyButton';
+import { useRouter } from 'expo-router';
+import { PlantCard } from '@/components/PlantCard';
 
 export default function App() {
+  const router = useRouter();
+  const plants = usePlantStore((state) => state.plants);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>This is a test</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      data={plants}
+      renderItem={({ item }) => <PlantCard plant={item} />}
+      ListEmptyComponent={
+        <PlantlyButton
+          title='Add your first plant'
+          onPress={() => {
+            router.navigate('/new');
+          }}
+        />
+      }
+    />
   );
 }
 
@@ -16,7 +31,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  contentContainer: {
+    padding: 12,
   },
 });
